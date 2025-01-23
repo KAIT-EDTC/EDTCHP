6c96109b0333102a45f2800c1b369c42bdf90655
fami-gb <fami.gb0825@gmail.com>
2025-01-24 05:33:29 +0900

    傲慢な修正した

--- register-form/datacheck.php
+++ register-form/datacheck.php
@@ -1,23 +1,22 @@
 <?php
-$servername = "mysql3105.db.sakura.ne.jp";
-$username = "kaitedtc_mamber-db";
-$password = "GU8-2bPQKYWP9m-";
-$dbname = "kaitedtc_mamber-db";
-$id = htmlspecialchars($_POST['ID']ENT_QUOTES, 'UTF-8');
-$pw = htmlspecialchars($_POST['pw']ENT_QUOTES, 'UTF-8');
+define('DSN', 'mysql:host=mysql3105.db.sakura.ne.jp;dbname=kaitedtc_mamber-db');
+define('DB_USERNAME', 'kaitedtc_mamber-db');
+define('DB_PASS', 'GU8-2bPQKYWP9m-');
+$id = htmlspecialchars($_POST['ID'], ENT_QUOTES, 'UTF-8');
+$pw = htmlspecialchars($_POST['pw'], ENT_QUOTES, 'UTF-8');
 $categories_string = implode(', ', array_map(function ($category) {
     return htmlspecialchars($category, ENT_QUOTES, 'UTF-8');
 }, $categories));
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO(DSN, DB_USERNAME, DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
-    $sql = "INSERT INTO login-data (name, pass) VALUES (:id,:pw)";
+    $sql = "INSERT INTO login-data (name, pass) VALUES (:id, :pw)";
     $stmt = $conn->prepare($sql);
 
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->bindParam(':pw', $pw, PDO::PARAM_STR); 
+    $stmt->bindParam(':pw', $pw, PDO::PARAM_STR);
 
     $stmt->execute();
 
