3d5e8fd892ef39ac8874706e860060d2a390307e
fami-gb <fami.gb0825@gmail.com>
2025-01-24 04:06:37 +0900

    fix: defineメソッドで簡略化

--- login-from/logincheck.php
+++ login-from/logincheck.php
@@ -1,8 +1,7 @@
 <?php
-$servername = "mysql3105.db.sakura.ne.jp";
-$username = "kaitedtc_mamber-db";
-$password = "GU8-2bPQKYWP9m-";
-$dbname = "kaitedtc_mamber-db";
+define('DSN', 'mysql:host=mysql3105.db.sakura.ne.jp;dbname=kaitedtc_mamber-db');
+define('DB_USERNAME', 'kaitedtc_mamber-db');
+define('DB_PASS', 'GU8-2bPQKYWP9m-');
 $id = htmlspecialchars($_POST['ID']ENT_QUOTES, 'UTF-8');
 $pw = htmlspecialchars($_POST['pw']ENT_QUOTES, 'UTF-8');
 $categories_string = implode(', ', array_map(function ($category) {
@@ -10,7 +9,7 @@ $categories_string = implode(', ', array_map(function ($category) {
 }, $categories));
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO(DSN, DB_USERNAME, DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     $sql = "SELECT * FROM login-data";
     $stmt = $conn->pquery($sql);
