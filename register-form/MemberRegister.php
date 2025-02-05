d349e9c9f2276bdb6d55668be391c76bf4c43ecd
yoyo <yoyo0612yuuki1101@gmail.com>
2025-02-05 19:08:07 +0900

    locahoに移行

--- register-form/MemberRegister.php
+++ register-form/MemberRegister.php
@@ -1,7 +1,7 @@
 <?php
-define('DSN', 'mysql:host=mysql3105.db.sakura.ne.jp;dbname=kaitedtc_mamber-db');
-define('DB_USERNAME', 'kaitedtc_mamber-db');
-define('DB_PASS', 'GU8-2bPQKYWP9m-');
+define('DSN', 'mysql:host=localhost;dbname=kaitedtc_mamber-db');
+define('DB_USERNAME', 'root');
+define('DB_PASS', '');
 
 try {
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -13,7 +13,7 @@ try {
     $conn = new PDO(DSN, DB_USERNAME, DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare('SELECT * FROM `login-data` WHERE SIDn = :id AND pass = :pass');
+    $stmt = $conn->prepare('INSERT INTO `login-data` (SIDn, pass) VALUES (:id, :pw)');
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->bindParam(':pass', $pw, PDO::PARAM_STR);
     $stmt->execute();
@@ -21,3 +21,14 @@ try {
     echo $e->getMessage();
 }
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登録画面</title>
+</head>
+<body>
+    <h1>登録画面</h1>
+</body>
+</html>
