1cba8b88d5beda247acfe4aafd0ff7e661c5549b
fami-gb <fami.gb0825@gmail.com>
2025-02-19 19:09:48 +0900

    feat: 予定の追加機能+予定の取得方法修正

--- mypage/calendar/calendar.php
+++ mypage/calendar/calendar.php
@@ -1,16 +1,12 @@
 <?php
 require_once __DIR__ . '/functions.php';
-// どんな予定を取得するか
-$json = getJson(10, 'startTime', true, '2024-01-01');
-$summary = getSchedule($json);
-// IDでフィルタリング
-$filtered[] = getScheduleById($summary, '2424013');
-
-// テスト用
+$summary = getEvents(10, 'startTime', true, '2024-01-01');
+$filtered[] = getEventsById($summary, '2424013');
 echo '<pre>';
 foreach ($filtered as $f) {
     var_dump($f);
 }
+addEvents();
 echo '</pre>';
 ?>
 
--- mypage/calendar/functions.php
+++ mypage/calendar/functions.php
@@ -1,35 +1,89 @@
 <?php
 // require_once __DIR__ . '/common.php';
-// 予定の取得
-require __DIR__ . '/vendor/autoload.php';
+
+use Google\Client;
+use Google\Service\Calendar as Google_Service_Calendar;
+use Google\Service\Calendar\Event as Google_Service_Calendar_Event;
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/google/apiclient-services/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+// envファイルで隠しとく
+$calendarId = $_ENV['CALENDAR_ID'];
+$api_key = $_ENV['API_KEY'];
+$jsonPath = __DIR__.'/key/push-event-test-451408-0f871f466586.json';
 
-function getJson($maxResults, $orderBy, $isSingleEvents, $timeMin) {
-    // envファイルで隠しとく
-    $calendarId = $_ENV['CALENDAR_ID'];
-    $api_key = $_ENV['API_KEY'];
-
-    // 取得時の詳細設定
-    $optParams = array(
-        'maxResults' => $maxResults,
-        'orderBy' => $orderBy,
-        'singleEvents' => $isSingleEvents,
-        'timeMin' => date('c', strtotime($timeMin))
-    );
-
-    $API_URL = 'https://www.googleapis.com/calendar/v3/calendars/'.$calendarId.'/events?key='.$api_key.'&singleEvents=true';
-
-    // 予定の取得クエリを合体
-    $url = $API_URL.'&'.implode('&', $optParams);
-
-    // urlからjsonを取得
-    $results = file_get_contents($url);
-    return  json_decode($results, true);
+function getClient($isAdd) {
+    global $jsonPath;
+    $client = new Google_Client();
+    if ($isAdd) $client->setScopes(\Google_Service_Calendar::CALENDAR_EVENTS);
+    else $client->setScopes(\Google_Service_Calendar::CALENDAR_READONLY);
+    $client->setAuthConfig($jsonPath);
+    return $client;
 }
 
-function getSchedule($json) {
-    foreach ($json['items'] as $item) {
+function addEvents() {
+    global $calendarId;
+    $EVENT_START_DATE = '2025-02-18T10:00:00+09:00';
+    $EVENT_END_DATE = '2025-02-18T11:00:00+09:00';
+
+    $client = getClient(true);
+
+    $service = new \Google_Service_Calendar($client);
+    $event = new \Google_Service_Calendar_Event(array(
+        'summary' => '遊行塾',
+        'description' => '2424013',
+        'start' => array(
+            'dateTime' => $EVENT_START_DATE,
+            'timeZone' => 'Asia/Tokyo',
+        ),
+        'end' => array(
+            'dateTime' => $EVENT_END_DATE,
+            'timeZone' => 'Asia/Tokyo',
+        ),
+    ));
+    return $service->events->insert($calendarId, $event);
+}
+
+// getClientしないやつ
+// function getJson($maxResults, $orderBy, $isSingleEvents, $timeMin) {
+//     global $calendarId, $api_key;
+
+//     // 取得時の詳細設定
+//     $optParams = array(
+//         'maxResults' => $maxResults,
+//         'orderBy' => $orderBy,
+//         'singleEvents' => $isSingleEvents,
+//         'timeMin' => date('c', strtotime($timeMin))
+//     );
+
+//     $queryParams = http_build_query($optParams);
+//     $API_URL = 'https://www.googleapis.com/calendar/v3/calendars/'.$calendarId.'/events?key='.$api_key;
+
+//     // 予定の取得クエリを合体
+//     $url = $API_URL.'&'.$queryParams;
+
+//     // urlからjsonを取得
+//     $results = file_get_contents($url);
+//     return  json_decode($results, true);
+// }
+
+function getEvents($maxResults, $orderBy, $isSingleEvents, $timeMin) { 
+    global $calendarId;
+    $events = [];
+    $optParams = array(
+                'maxResults' => $maxResults,
+                'orderBy' => $orderBy,
+                'singleEvents' => $isSingleEvents,
+                'timeMin' => date('c', strtotime($timeMin))
+    );
+
+    $client = getClient(false);
+    $service = new Google_Service_Calendar($client);
+    $results = $service->events->listEvents($calendarId, $optParams);
+    $events = $results->getItems();
+    foreach ($events as $item) {
         /*
         * 基本的なキーワード
         * created : 作成日
@@ -38,18 +92,19 @@ function getSchedule($json) {
         * end : 終了日時
         * description : 詳細
         */
-        // 新たに配列を作って良い感じに整形(項目はもっと増やせる)
-        $schedule[] = array(
+        // こんな感じで予定の取得が出来る
+        $formatted_events[] = array(
             'summary' => $item['summary'],
-            'startTime' => $item['start'],
+            'startTime' => $item['start']['dateTime'],
+            'endTime' => $item['end']['dateTime'],
             'description' => $item['description']
         );
     }
-    return $schedule;
+    return $formatted_events;
 }
 
-// 指定されたIDを含む予定を返す
-function getScheduleById($data, $Id) {
+function getEventsById($data, $Id) {
+    $filtered_data = [];
     foreach ($data as $d) {
         if (strpos($d['description'], $Id) !== false) {
             $filtered_data[] = $d;
