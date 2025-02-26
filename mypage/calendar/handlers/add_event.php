17d9fd13ad54490dc42d7aa215e8e5fe888d2eed
fami-gb <fami.gb0825@gmail.com>
2025-02-26 13:40:51 +0900

    feat: 予定追加時にDBにデータを送信

--- mypage/calendar/handlers/add_event.php
+++ mypage/calendar/handlers/add_event.php
@@ -1,5 +1,10 @@
 <?php
 require_once __DIR__ . '/../functions.php';
+define('DSN', 'mysql:host=localhost;dbname=test');
+define('DB_USERNAME', '');
+define('DB_PASS', '');
+$conn = new PDO(DSN, DB_USERNAME, DB_PASS);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = htmlspecialchars($_POST['title'], ENT_QUOTES, 'UTF-8');
     $remark = htmlspecialchars($_POST['remark'], ENT_QUOTES, 'UTF-8');
@@ -8,6 +13,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $participants = htmlspecialchars($_POST['participants'], ENT_QUOTES, 'UTF-8');
 
     addEvents($title, $remark, $start, $end, $participants);
+    $stmt = $conn->prepare('INSERT INTO `yotei-data` (title, remark, start, end, part) VALUES (:title, :remark, :start, :end, :part)');
+    $stmt->execute([
+        ':title'    => $title,
+        ':remark'   => $remark,
+        ':start'    => $start,
+        ':end'      => $end,
+        ':part'     => $participants
+    ]);
+
     header('Location: ../calendar.php');
+    $conn = null;
 }
 ?>
