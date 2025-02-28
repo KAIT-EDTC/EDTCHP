d34084ee11c245ec826d1055690482124479b9a4
fami-gb <fami.gb0825@gmail.com>
2025-02-28 16:32:32 +0900

    feat: 記事の概要を記載

--- js/makeArticle.js
+++ js/makeArticle.js
@@ -1,9 +1,9 @@
 const content = [
-    { date: "2024-06-22", title: "第五回遊行塾に行ってきました", img: "blog/blog-img/24-0622-yugyou1.jpg", caption: "記事の概略", author: "山口", link: "blog/blog-data/yugyou5-blog.html" },
-    { date: "2024-06-29", title: "神奈川工科大学見学体験ツアーを行いました", img: "blog/blog-img/24-0629-asahi1.png", caption: "記事の概略", author: "山口", link: "blog/blog-data/asahi.blog.html" },
-    { date: "2024-07-11", title: "地域連携サークル交流会に参加しました", img: "blog/blog-img/24-0711-kouryu.jpg", caption: "記事の概略", author: "山口", link: "blog/blog-data/kouryukai.html" },
-    { date: "2024-09-28", title: "第六回遊行塾に行ってきました", img: "blog/blog-img/24-0928-yugyou6.jpg", caption: "記事の概略", author: "根本", link: "blog/blog-data/yugyou6-blog.html" },
-    { date: "2024-10-28", title: "test", img: "blog/blog-img/24-0928-yugyou6.jpg", caption: "記事の概略", author: "根本", link: "blog/blog-data/yugyou6-blog.html" },
+    { date: "2024-06-22", title: "第五回遊行塾に行ってきました", img: "blog/blog-img/24-0622-yugyou1.jpg", caption: "前期最終回となる遊行塾でしたが、楽しく学べる授業を提供できたと思います。<br>当サイト初のブログになりますので至らぬ点もあるかと思いますが読んでいただけたら幸いです。", author: "山口", link: "blog/blog-data/yugyou5-blog.html" },
+    { date: "2024-06-29", title: "神奈川工科大学見学体験ツアーを行いました", img: "blog/blog-img/24-0629-asahi1.png", caption: "本年度二回目となるあさひ学苑さんとのイベントを行いました。<br>今回も弊学に来ていただき学内を探索したり工房にて缶バッチ制作をしていただきました", author: "山口", link: "blog/blog-data/asahi.blog.html" },
+    { date: "2024-07-11", title: "地域連携サークル交流会に参加しました", img: "blog/blog-img/24-0711-kouryu.jpg", caption: "今回はKAIT内部のサークルの集い地域連携サークル交流会にご招待いただいたので参加してきました。", author: "山口", link: "blog/blog-data/kouryukai.html" },
+    { date: "2024-09-28", title: "第六回遊行塾に行ってきました", img: "blog/blog-img/24-0928-yugyou6.jpg", caption: "嶺学園藤沢中学校に第六回遊行塾を実施しました。<br>後期始めにふさわしい快調なスタートを切ることが出来ました", author: "根本", link: "blog/blog-data/yugyou6-blog.html" },
+    // { date: "2024-10-28", title: "test", img: "blog/blog-img/24-0928-yugyou6.jpg", caption: "記事の概略", author: "根本", link: "blog/blog-data/yugyou6-blog.html" },
 ];
 
 const articleLoad = () => {
@@ -38,7 +38,7 @@ const articleLoad = () => {
 
         const captionElement = document.createElement("p");
         captionElement.className = "blog-caption";
-        captionElement.innerText = article.caption;
+        captionElement.innerHTML = article.caption;
 
         const authorElement = document.createElement("p");
         authorElement.className = "blog-author";
