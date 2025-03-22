a146a35022c6687228411d2f71580ea79752d878
fami-gb <92741782+fami-gb@users.noreply.github.com>
2025-03-22 16:59:04 +0900

    Update MemberRegister.php

--- register-form/MemberRegister.php
+++ register-form/MemberRegister.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/EDTCHP/Pjinfo.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Pjinfo.php';
 
 try {
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
