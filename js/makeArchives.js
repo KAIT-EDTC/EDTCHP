0c51ff65b7d65c8fa47047d2ee593b7755d88093
fami-gb <fami.gb0825@gmail.com>
2025-04-20 12:30:47 +0900

    fix:ブログのデータを一元管理

--- js/makeArchives.js
+++ js/makeArchives.js
@@ -1,49 +1,5 @@
-const content = [
-    {
-        date: "2024-06-22",
-        title: "第五回遊行塾に行ってきました",
-        img: "blog/blog-img/24-0622-yugyou1.jpg",
-        caption: "前期最終回となる遊行塾でしたが、楽しく学べる授業を提供できたと思います。\
-                当サイト初のブログになりますので至らぬ点もあるかと思いますが読んでいただけたら幸いです。",
-        author: "山口",
-        link: "yugyou5-blog.html"
-    },
-    { 
-        date: "2024-06-29", 
-        title: "神奈川工科大学見学体験ツアーを行いました", 
-        img: "blog/blog-img/24-0629-asahi1.png", 
-        caption: "本年度二回目となるあさひ学苑さんとのイベントを行いました。\
-                今回も弊学に来ていただき学内を探索したり工房にて缶バッチ制作をしていただきました", 
-        author: "山口", 
-        link: "asahi.blog.html" 
-    },
-    { 
-        date: "2024-07-11", 
-        title: "地域連携サークル交流会に参加しました", 
-        img: "blog/blog-img/24-0711-kouryu.jpg", 
-        caption: "今回はKAIT内部のサークルの集い地域連携サークル交流会にご招待いただいたので参加してきました。", 
-        author: "山口", 
-        link: "kouryukai.html" 
-    },
-    { 
-        date: "2024-09-28", 
-        title: "第六回遊行塾に行ってきました", 
-        img: "blog/blog-img/24-0928-yugyou6.jpg", 
-        caption: "嶺学園藤沢中学校に第六回遊行塾を実施しました。後期始めにふさわしい快調なスタートを切ることが出来ました", 
-        author: "根本", 
-        link: "yugyou6-blog.html" 
-    },
-    { 
-        date: "2024-12-21", 
-        title: "放課後等デイサービスあさひ学苑IT校様が、神奈川工科大学に遊びに来てくれました！", 
-        img: "blog/blog-img/IMG_8304.jpg", 
-        caption: "12月に放課後等デイサービスあさひ学苑IT校様が、神奈川工科大学に遊びに来てくれました！\
-                今年度第3回目となる今回は大学探検やKAIT工房での木工体験です。 ", 
-        author: "番倉", 
-        link: "asahi-IT.html" 
-    },
-    // { date: "2024-10-28", title: "test", img: "blog/blog-img/24-0928-yugyou6.jpg", caption: "記事の概略", author: "根本", link: "blog/blog-data/yugyou6-blog.html" },
-];
+import { content } from "./articleData.js";
+
 const articleLoad = () => {
     const Archives = document.getElementsByClassName("Archives")[0];
     const ulElement = document.createElement("ul");
@@ -52,6 +8,7 @@ const articleLoad = () => {
         const boxElement = document.createElement("li");
 
         const AnkerElement = document.createElement("a");
+        article.link = article.link.replace(/blog\/blog-data/, ".");
         AnkerElement.href = article.link;
 
         const titleElement = document.createElement("span");
@@ -90,4 +47,4 @@ const articleLoad = () => {
 //     liArray.forEach(li => ulElement.appendChild(li));
 // };
 
-document.addEventListener("DOMContentLoaded", articleLoad());
+document.addEventListener("DOMContentLoaded", () => articleLoad());
