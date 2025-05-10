0d2d1c83988e11a1b02f515a1c8eb02a531511f2
fami-gb <fami.gb0825@gmail.com>
2025-05-11 03:07:33 +0900

    fix:商品の画像の更新 fix:商品詳細ページのタイトル修正

--- data/pdctData.js
+++ data/pdctData.js
@@ -8,7 +8,7 @@ export const pdct_list = [
     {
         name: "防犯ブザー",
         headline: "本格的な機械工作体験をしてみませんか？<br>作りながら防犯ブザーの仕組みを理解しよう！",
-        src: ALTER_IMAGE_PATH,
+        src: IMAGE_DIR_PATH + "buzzer.jpg",
         file: "product-buzzer.html",
         tags: ["1000円", "全年齢"],
         maker: "足立遥大"
@@ -16,15 +16,15 @@ export const pdct_list = [
     {
         name: "ArtoRo(アトロ)",
         headline: "まるで動物のようについてくるロボット！<br>仕組みから学ぶ、ロボット製作体験！",
-        src: IMAGE_DIR_PATH + "ArtoRo.png",
+        src: IMAGE_DIR_PATH + "ArtoRo-front.jpg",
         file: "product-ArtoRo.html",
         tags: ["未定", "中学生"],
         maker: "番倉もえ"
     },
     {
-        name: "LogicLineTracer",
+        name: "LogicLineTracerV2",
         headline: "安価に短時間で作れる！<br>ロジックICを使ったライントレーサー",
-        src: IMAGE_DIR_PATH + "LogicLineTracer.jpg",
+        src: IMAGE_DIR_PATH + "LogicLineTracerV2.jpg",
         file: "product-LogicLineTracer.html",
         tags: ["1000円～1500円", "高校生"],
         maker: "須藤陸"
@@ -40,7 +40,7 @@ export const pdct_list = [
     {
         name: "ぶるぶるくん",
         headline: "虫のように振動する！？<br>モーターを使った不思議なロボット",
-        src: ALTER_IMAGE_PATH,
+        src: IMAGE_DIR_PATH + "buruburu.jpg",
         file: "product-buruburu.html",
         tags: ["500円", "小学生"],
         maker: "鈴木一平"
