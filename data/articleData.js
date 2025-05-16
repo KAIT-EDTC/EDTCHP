5583824db4db73d3a7b1dd21743a5636a8efd68d
fami-gb <fami.gb0825@gmail.com>
2025-05-16 18:48:24 +0900

    hotfix

--- data/articleData.js
+++ data/articleData.js
@@ -3,7 +3,7 @@ export const content = [
         date: "2024-06-22",
         title: "第五回遊行塾に行ってきました！",
         img: "blog/blog-img/24-0622-yugyou1.jpg",
-        caption: "前期最終回となる遊行塾でしたが、楽しく学べる授業を提供できたと思います。\
+        caption: "前期最終回となる遊行塾でしたが、楽しく学べる授業を提供できたと思います。<br>\
                 当サイト初のブログになりますので至らぬ点もあるかと思いますが読んでいただけたら幸いです。",
         author: "山口",
         link: "blog/blog-data/yugyou5-blog.html"
@@ -12,7 +12,7 @@ export const content = [
         date: "2024-06-29", 
         title: "神奈川工科大学見学体験ツアーを行いました", 
         img: "blog/blog-img/24-0629-asahi1.png", 
-        caption: "本年度二回目となるあさひ学苑さんとのイベントを行いました。\
+        caption: "本年度二回目となるあさひ学苑さんとのイベントを行いました。<br>\
                 今回も弊学に来ていただき学内を探索したり工房にて缶バッチ制作をしていただきました", 
         author: "山口", 
         link: "blog/blog-data/asahi.blog.html" 
@@ -22,14 +22,14 @@ export const content = [
         title: "地域連携サークル交流会に参加しました", 
         img: "blog/blog-img/24-0711-kouryu.jpg", 
         caption: "今回はKAIT内部のサークルの集い地域連携サークル交流会にご招待いただいたので参加してきました。", 
-        author: "山口", 
+        author: "足立", 
         link: "blog/blog-data/kouryukai.html" 
     },
     { 
         date: "2024-09-28", 
         title: "第六回遊行塾に行ってきました！", 
         img: "blog/blog-img/24-0928-yugyou6.jpg", 
-        caption: "嶺学園藤沢中学校に第六回遊行塾を実施しました。後期始めにふさわしい快調なスタートを切ることが出来ました", 
+        caption: "嶺学園藤沢中学校に第六回遊行塾を実施しました。<br>後期始めにふさわしい快調なスタートを切ることが出来ました", 
         author: "根本", 
         link: "blog/blog-data/yugyou6-blog.html" 
     },
@@ -37,8 +37,7 @@ export const content = [
         date: "2024-12-21", 
         title: "放課後等デイサービスあさひ学苑IT校様が、神奈川工科大学に遊びに来てくれました！", 
         img: "blog/blog-img/IMG_8304.jpg", 
-        caption: "12月に放課後等デイサービスあさひ学苑IT校様が、神奈川工科大学に遊びに来てくれました！\
-                今年度第3回目となる今回は大学探検やKAIT工房での木工体験です。 ", 
+        caption: "今年度第3回目となる今回は大学探検やKAIT工房での木工体験を行いました！ ", 
         author: "番倉", 
         link: "blog/blog-data/asahi-IT.html" 
     },
