82de4f4e9a9e2ed9f503ef6bc0fad76d0ececa28
yoyo <yoyo0612yuuki1101@gmail.com>
2025-05-20 15:43:53 +0900

    regist fix

--- register-form/MemberRegister.php
+++ register-form/MemberRegister.php
@@ -16,13 +16,15 @@ try {
     }
     $id = htmlspecialchars($_POST['ID'], ENT_QUOTES, 'UTF-8');
     $pw = htmlspecialchars($_POST['pw'], ENT_QUOTES, 'UTF-8');
+    $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
     
     $conn = new PDO(DSN, DB_USERNAME, DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare('INSERT INTO `login-data` (pass,SIDn) VALUES (:pass,:id)');
+    $stmt = $conn->prepare('INSERT INTO `login-data` (pass,SIDn,name) VALUES (:pass,:id,:name)');
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->bindParam(':pass', $pw, PDO::PARAM_STR);
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
     $stmt->execute();
     header('Location: ' . REGISTER_FORM);
 } catch (PDOException $e) {
---
+++ register-form/ScheduleRegister.php
@@ -0,0 +1,36 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Pjinfo.php';
+
+try {
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        header('Location: ' . LOGIN_FORM);
+        exit;
+    }
+    $evname = htmlspecialchars($_POST['evid'], ENT_QUOTES, 'UTF-8');
+    $date = htmlspecialchars($_POST['date'], ENT_QUOTES, 'UTF-8');
+    $member = htmlspecialchars($_POST['member'], ENT_QUOTES, 'UTF-8');
+    
+    $conn = new PDO(DSN, DB_USERNAME, DB_PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare('INSERT INTO `yotei-data` (date, member, name) VALUES (:date, :member, :name)');
+    $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+    $stmt->bindParam(':member', $member, PDO::PARAM_STR);
+    $stmt->bindParam(':name', $evname, PDO::PARAM_STR);
+    $stmt->execute();
+    header('Location: ' . REGISTER_FORM);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登録画面</title>
+</head>
+<body>
+    <h1>登録画面</h1>
+</body>
+</html>
