dcd73a4ab387eb13aa552aaf0f64215c099ad72b
yoyo <yoyo0612yuuki1101@gmail.com>
2025-05-22 13:57:05 +0900

    blog fix

--- data/articleData.js
+++ data/articleData.js
@@ -6,7 +6,7 @@ export const content = [
         caption: "前期最終回となる遊行塾でしたが、楽しく学べる授業を提供できたと思います。<br>\
                 当サイト初のブログになりますので至らぬ点もあるかと思いますが読んでいただけたら幸いです。",
         author: "山口",
-        link: "blog/blog-data/yugyou5-blog.html"
+        link: "blog/blog-data/24-06-22yugyou5-blog.html"
     },
     { 
         date: "2024-06-29", 
@@ -15,7 +15,7 @@ export const content = [
         caption: "本年度二回目となるあさひ学苑さんとのイベントを行いました。<br>\
                 今回も弊学に来ていただき学内を探索したり工房にて缶バッチ制作をしていただきました", 
         author: "山口", 
-        link: "blog/blog-data/asahi.blog.html" 
+        link: "blog/blog-data/24-06-29asahi.blog.html" 
     },
     { 
         date: "2024-07-11", 
@@ -23,7 +23,7 @@ export const content = [
         img: "blog/blog-img/24-0711-kouryu.jpg", 
         caption: "今回はKAIT内部のサークルの集い地域連携サークル交流会にご招待いただいたので参加してきました。", 
         author: "足立", 
-        link: "blog/blog-data/kouryukai.html" 
+        link: "blog/blog-data/24-07-11kouryukai.html" 
     },
     { 
         date: "2024-09-28", 
@@ -31,7 +31,7 @@ export const content = [
         img: "blog/blog-img/24-0928-yugyou6.jpg", 
         caption: "嶺学園藤沢中学校に第六回遊行塾を実施しました。<br>後期始めにふさわしい快調なスタートを切ることが出来ました", 
         author: "根本", 
-        link: "blog/blog-data/yugyou6-blog.html" 
+        link: "blog/blog-data/24-09-28yugyou6-blog.html" 
     },
     { 
         date: "2024-12-21", 
@@ -39,7 +39,7 @@ export const content = [
         img: "blog/blog-img/IMG_8304.jpg", 
         caption: "今年度第3回目となる今回は大学探検やKAIT工房での木工体験を行いました！ ", 
         author: "番倉", 
-        link: "blog/blog-data/asahi-IT.html" 
+        link: "blog/blog-data/24-12-21asahi-IT.html" 
     },
     { 
         date: "2024-12-08", 
@@ -48,7 +48,7 @@ export const content = [
         caption: "12月8日サイエンスウィンターに参加しました。\
             厚木市子ども科学館にて行われたサイエンスウィンターに参加しました。", 
         author: "棚橋", 
-        link: "blog/blog-data/2024siencewinter.html" 
+        link: "blog/blog-data/24-12-08siencewinter.html" 
     },
     // { 
     //     date: "2025-02-07", 
