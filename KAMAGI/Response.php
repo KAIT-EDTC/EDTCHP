b938e50020fd919d6f99fac83c3e510e75448ef5
fami-gb <fami.gb0825@gmail.com>
2025-11-23 23:28:23 +0900

    feat: Responseクラス作成

---
+++ KAMAGI/Response.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KAMAGI;
+
+class Response
+{
+    /**
+     * JSONレスポンスを送信して終了する
+     *
+     * @param int $statusCode HTTPステータスコード
+     * @param array $data レスポンスデータ
+     * @return void
+     */
+    public static function json(int $statusCode, array $data): void
+    {
+        http_response_code($statusCode);
+        header('Content-Type: application/json; charset=UTF-8');
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        exit;
+    }
+}
