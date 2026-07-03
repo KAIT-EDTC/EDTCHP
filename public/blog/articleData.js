f574fed318905df103d1a00e73faa64baae22060
DAIKON-SIRISIRI\Aonuma <yudetamoyasi@gmail.com>
2026-07-03 20:45:37 +0900

    feat: add blog post content and associated images for the June 26th entry

--- public/blog/articleData.js
+++ public/blog/articleData.js
@@ -19,6 +19,7 @@ export const BLOG_TAGS = [
 export const articleIds = [
     "26-05-16-yugyou01",
     "26-06-12-yugyou03",
+    "26-06-26-yugyou04",
     "25-12-07-sciencewinter",
     "25-10-26-toureisai",
     "25-09-27-yugyou06",
